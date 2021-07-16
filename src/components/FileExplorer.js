74530ecf89b9db6540ccef0f2c71cc895296d90e
Gokul Madhusudhan <gokul99.gm@gmail.com>
2021-07-16 17:04:06 +0530

    added : syntax highlighting

--- src/components/FileExplorer.js
+++ src/components/FileExplorer.js
@@ -1,29 +1,67 @@
-import React, { useState, useCallback, useEffect } from 'react'
+import React, { useState } from 'react'
 import LiveView from './LiveView';
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-markup';
+import './prism.css';
+
+require('prismjs/components/prism-jsx');
+require('prismjs/components/prism-css');
 
 export default function FileExplorer() {
 
   const [html, setHtml] = useState('')
   const [css, setCss] = useState('')
   const [js, setJs] = useState('')
-  // const [userText, setUserText] = useState('');
-
-
-  // useEffect(() => {
-  //   window.addEventListener('keydown', handleUserKeyPress);
-  //   return () => {
-  //     window.removeEventListener('keydown', handleUserKeyPress);
-  //   };
-  // }, [handleUserKeyPress]);
 
   return (
     <div className="page-wrap twilight">
       <div className="boxes">
         <div className="coding_area">
-          <textarea id="html" placeholder="HTML" className="prism-live language-html"
-            value={html} onChange={e => setHtml(e.target.value)}></textarea>
-          <textarea id="css" placeholder="CSS" className="prism-live language-css" value={css} onChange={e => setCss(e.target.value)}></textarea>
-          <textarea id="js" placeholder="JS" className="prism-live language-js" value={js} onChange={e => setJs(e.target.value)}></textarea>
+          <Editor
+            placeholder="HTML"
+            value={html}
+            onValueChange={code => setHtml(code)}
+            highlight={code => highlight(code, languages.jsx)}
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 12,
+              color: 'white',
+              backgroundColor: '#333234'
+            }}
+            className="prism"
+          />
+          <Editor
+            placeholder="CSS"
+            value={css}
+            onValueChange={code => setCss(code)}
+            highlight={code => highlight(code, languages.css)}
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 12,
+              color: 'white',
+              backgroundColor: '#333234'
+            }}
+            className="prism"
+          />
+          <Editor
+            placeholder="JS"
+            value={js}
+            onValueChange={code => setJs(code)}
+            highlight={code => highlight(code, languages.js)}
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 12,
+              color: 'white',
+              backgroundColor: '#333234'
+            }}
+            className="prism"
+          />
         </div>
         <LiveView code={`${html}<style>${css}</style><script>${js}</script>`}>
         </LiveView>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import FileExplorer from './FileExplorer'
-import LiveView from './LiveView'
 
 function Main() {
   return (
@@ -9,7 +8,6 @@ function Main() {
         <img id="logo" src="https://www.dyte.io/images/Dyte-Logo.svg" />
       </header>
       <FileExplorer />
-      {/* // <LiveView /> */}
     </div>
   )
 }
